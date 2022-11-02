7a61cc131fba78b76140d661c073f42e966c58f5
RodionAparin <rodionaparing@coherentsolutions.com>
2022-11-02 10:40:35 +0300

    asd

--- task-4-1/src/test/java/by/bsu/factorialcalculator/FactorialCalculatorTest.java
+++ task-4-1/src/test/java/by/bsu/factorialcalculator/FactorialCalculatorTest.java
@@ -1,37 +1,52 @@
 package by.bsu.factorialcalculator;
 
 import by.bsu.exception.IncorrectParameter;
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FactorialCalculatorTest extends TestCase {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FactorialCalculatorTest {
     private final FactorialCalculator calculator = new FactorialCalculator();
 
-
-    public void testGetFirstNFactorials() throws IncorrectParameter {
+    @Test
+    public void getFirst0Factorials_ReturnsList() throws IncorrectParameter {
         int i = 0;
         List<BigInteger> result = calculator.getFirstNFactorials(i);
         List<BigInteger> expected = new ArrayList<>();
 
         assertEquals(expected, result);
+    }
 
-        i = 1;
-        result = calculator.getFirstNFactorials(i);
-        expected = List.of(BigInteger.ONE);
+    @Test
+    public void getFirst1Factorials_ReturnsList() throws IncorrectParameter {
+        int i = 1;
+        List<BigInteger> result = calculator.getFirstNFactorials(i);
+        List<BigInteger> expected = List.of(BigInteger.ONE);
 
         assertEquals(expected, result);
+    }
 
-        i = 3;
-        result = calculator.getFirstNFactorials(i);
-        expected = List.of(
+    @Test
+    public void getFirst3Factorials_ReturnsList() throws IncorrectParameter {
+        int i = 3;
+        List<BigInteger> result = calculator.getFirstNFactorials(i);
+        List<BigInteger> expected = List.of(
                 BigInteger.ONE,
                 BigInteger.TWO,
                 BigInteger.TWO.multiply(BigInteger.valueOf(3))
         );
 
         assertEquals(expected, result);
+    }
+
+    @Test
+    public void getFirstNFactorials_ReturnsException() {
+        int i = -100;
+        assertThrows(IncorrectParameter.class, () -> calculator.getFirstNFactorials(i));
     }
 }
